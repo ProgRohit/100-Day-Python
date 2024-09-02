07a2673b615d6a11ffb971805df82b7ed7ee91e3
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-02 17:32:32 +0530

    Create Problem4.py

---
+++ Day 1/Problem4.py
@@ -0,0 +1,7 @@
+# Question: Write a program that takes an integer input from the user and prints whether the number is even or odd.
+
+n = int(input("Enter a number: "))
+if n % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
