72954d8d81c6f5cfd5fcb546dd0fe6833df03dcb
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-03 10:55:20 +0530

    Create Problem3.py

---
+++ Day 2/Problem3.py
@@ -0,0 +1,7 @@
+# Question: Write a Python program to access and print the first and last characters of a string provided by the user.
+
+s = input("Enter name: ")
+first = s[0]
+last = s[-1]
+print('first character: ', first)
+print('last character: ', last)
