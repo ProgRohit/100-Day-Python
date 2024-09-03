1f20b1df0daa43474ded19330a57514b20ae0e3f
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-03 10:56:24 +0530

    Create Problem5.py

---
+++ Day 2/Problem5.py
@@ -0,0 +1,5 @@
+# Question: Write a Python program to extract a substring from a string provided by the user. The substring should be from the 2nd character to the 5th character (inclusive).
+
+s = input("Enter string: ")
+ss = s[1:5]
+print('Substring: ', ss)
