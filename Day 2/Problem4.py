3fb9727c7a2fe41fde3e7891a00222e8d57fd42e
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-03 10:55:53 +0530

    Create Problem4.py

---
+++ Day 2/Problem4.py
@@ -0,0 +1,7 @@
+# Question: Write a Python program to convert a string provided by the user to both uppercase and lowercase.
+
+s = input("Enter name: ")
+uc = s.upper()
+lc = s.lower()
+print('Uppercase: ', uc)
+print('Lowercase: ', lc)
