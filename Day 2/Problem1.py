a2bf8a64a9b3400e5822e62d34f0a7a7f421a0af
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-03 10:54:14 +0530

    Create Problem1.py

---
+++ Day 2/Problem1.py
@@ -0,0 +1,7 @@
+# Question: Write a Python program that concatenates two strings provided by the user.
+
+string1 = input('Enter first name: ')
+string2 = input('Enter last name: ')
+space = ' '
+full_name = string1 + space + string2
+print('Your name is: ', full_name)
