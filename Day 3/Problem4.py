dfa6e3bc90b56f0bb8d990b82d238b684e3eb051
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-04 10:07:16 +0530

    Create Problem4.py

---
+++ Day 3/Problem4.py
@@ -0,0 +1,5 @@
+# Question: Write a Python program that formats a string using f-strings. The program should take a user’s name and age as inputs and display a formatted message.
+
+name = input('Enter Name: ')
+age = int(input('Enter Age: '))
+print(f'Hello, {name}! You are {age} years old.')
