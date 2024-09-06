384014898ca4693d3966fcd1c22455fb81ee1121
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-06 09:45:09 +0530

    Create Problem3.py

---
+++ Day 5/Problem3.py
@@ -0,0 +1,7 @@
+# Question: Write a Python program that takes a list of numbers and calculates the sum of all elements in the list.
+
+l = [1, 2, 3, 4, 5]
+sum = 0
+for num in l:
+    sum = sum + num
+print(sum)
