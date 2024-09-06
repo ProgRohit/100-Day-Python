20816ea156dc9179954970aaf6436e2161fe9905
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-06 09:46:15 +0530

    Create Problem4.py

---
+++ Day 5/Problem4.py
@@ -0,0 +1,8 @@
+# Question: Write a Python program to check if a particular element exists in a given list.
+
+l = [1, 2, 3, 4, 5]
+element = 5
+if element in l:
+    print('Element exists!')
+else:
+    print('Element does not exist!')
