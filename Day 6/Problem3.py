e6293fb38838f1dfc8f64a2319e8b8dda4c7354e
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-07 20:43:21 +0530

    Create Problem3.py

---
+++ Day 6/Problem3.py
@@ -0,0 +1,9 @@
+# Question: Check if a Number is Positive, Negative, or Zero
+
+n = int(input('Enter Number: '))
+if n == 0:
+    print('Number is 0')
+elif n > 0:
+    print('Number is Positive')
+else:
+    print('Number is Negative')
