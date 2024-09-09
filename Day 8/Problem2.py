f0048986f00384119c0d7fc38416f5404b7939fe
ProgRohit <158342176+ProgRohit@users.noreply.github.com>
2024-09-09 12:20:50 +0530

    Create Problem2.py

---
+++ Day 8/Problem2.py
@@ -0,0 +1,9 @@
+# Question: Function to Add Two Numbers
+
+def add(a, b):
+    return a + b
+
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+result = add(a, b)
+print(f'The sum is: {result}')
